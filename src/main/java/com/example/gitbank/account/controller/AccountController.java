14ca8eb86f5a4ada7d4995a0b76897a342a6db16
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-08-01 21:53:53 +0300

    Initialize account module

---
+++ src/main/java/com/example/gitbank/account/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.example.gitbank.account.controller;
+
+import com.example.gitbank.account.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+}
---
+++ src/main/java/com/example/gitbank/account/model/Account.java
@@ -0,0 +1,35 @@
+package com.example.gitbank.account.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", unique = false, nullable = false)
+    private String id;
+
+    private String customerId;
+
+    private Double balance;
+
+    private Currency currency;
+}
