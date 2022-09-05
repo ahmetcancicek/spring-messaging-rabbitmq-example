f2f0252007536c295b86e9e38563cfffb6108358
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-09-05 20:35:57 +0300

    Add new method for data access layer at customer domain

--- src/main/java/com/example/gitbank/customer/repository/CustomerRepository.java
+++ src/main/java/com/example/gitbank/customer/repository/CustomerRepository.java
@@ -4,6 +4,15 @@ import com.example.gitbank.customer.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+    Optional<Customer> findBySecurityNo(String securityNo);
+
+    @Override
+    Optional<Customer> findById(String id);
+
+    Boolean existsBySecurityNo(String securityNo);
 }
