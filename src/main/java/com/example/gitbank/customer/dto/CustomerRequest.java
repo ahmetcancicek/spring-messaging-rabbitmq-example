c116b994d5f05c59de54911d1a7e58e2eef6f4fb
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-09-08 20:27:45 +0300

    Add validation for DTO

--- src/main/java/com/example/gitbank/customer/dto/CustomerRequest.java
+++ src/main/java/com/example/gitbank/customer/dto/CustomerRequest.java
@@ -1,9 +1,10 @@
 package com.example.gitbank.customer.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Getter
 @Setter
@@ -12,8 +13,16 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerRequest {
+    @NotBlank
     private String securityNo;
+
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @NotBlank
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
 }
