a5ee84005f49b885b0d7890a010b79a165b4287a
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-09-13 13:21:55 +0300

    Fix entity for customer

---
+++ src/main/java/com/example/gitbank/notification/messaging/Customer.java
@@ -0,0 +1,11 @@
+package com.example.gitbank.notification.messaging;
+
+import java.time.LocalDate;
+
+public class Customer {
+    private String id;
+    private String securityNo;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+}
