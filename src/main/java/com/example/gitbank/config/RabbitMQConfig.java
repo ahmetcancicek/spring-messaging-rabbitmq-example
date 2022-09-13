ff9348eed393ba22ebd8da65cb51bedfa0710238
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-09-13 13:22:26 +0300

    Refactored configuration for RabbitMQ

--- src/main/java/com/example/gitbank/config/RabbitMQConfig.java
+++ src/main/java/com/example/gitbank/config/RabbitMQConfig.java
@@ -10,18 +10,19 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     public static final String EXCHANGE_CUSTOMER_TOPIC = "x.customer";
-    public static final String ROUTING_KEY_CUSTOMER_CREATED = "customer.event.created";
+    public static final String ROUTING_KEY_CUSTOMER_CREATED = "customer.created";
     public static final String QUEUE_CUSTOMER_CREATED = "q.customer-created-queue";
 
 
     public static final String EXCHANGE_ACCOUNT_DIRECT = "x.account";
-    public static final String ROUTING_KEY_ACCOUNT_CREATED = "account.event.created";
+    public static final String ROUTING_KEY_ACCOUNT_CREATED = "account..created";
     public static final String QUEUE_ACCOUNT_CREATED = "q.account-created-queue";
 
     @Bean
     public TopicExchange exchangeCustomerTopic() {
         return new TopicExchange(EXCHANGE_CUSTOMER_TOPIC);
     }
+
     @Bean
     public Queue queueCustomerCreated() {
         return new Queue(QUEUE_CUSTOMER_CREATED);
@@ -29,10 +30,23 @@ public class RabbitMQConfig {
 
     @Bean
     public Declarables topicExchangeBindings(TopicExchange exchangeCustomerTopic,
-                                             Queue queueCustomerCreated) {
+                                             DirectExchange exchangeAccountDirect,
+                                             Queue queueCustomerCreated,
+                                             Queue queueAccountCreated) {
         return new Declarables(
-            BindingBuilder.bind(queueCustomerCreated).to(exchangeCustomerTopic).with(ROUTING_KEY_CUSTOMER_CREATED)
+                BindingBuilder.bind(queueCustomerCreated).to(exchangeCustomerTopic).with(ROUTING_KEY_CUSTOMER_CREATED),
+                BindingBuilder.bind(queueAccountCreated).to(exchangeAccountDirect).with(ROUTING_KEY_ACCOUNT_CREATED)
         );
+    }
+
+    @Bean
+    public DirectExchange exchangeAccountTopic() {
+        return new DirectExchange(EXCHANGE_ACCOUNT_DIRECT);
+    }
+
+    @Bean
+    public Queue queueAccountCreated() {
+        return new Queue(QUEUE_ACCOUNT_CREATED);
     }
 
     @Bean
